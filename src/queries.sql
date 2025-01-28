02176025b5d70c85e40b58bd2be90da0481e6722
andre pereira <andrepeersil@gmail.com>
2025-01-28 20:13:01 -0300

    txfem e tfnaobrancos adicionadas

--- src/queries.sql
+++ src/queries.sql
@@ -3,6 +3,7 @@
 with tb_partidos as (
 
     select SG_PARTIDO,
+        SG_UF,
         NM_PARTIDO,
         count(SG_PARTIDO) as candidaturas
 
@@ -15,6 +16,7 @@ with tb_partidos as (
 tb_genero as (
     
     select SG_PARTIDO,
+        SG_UF,
         SQ_CANDIDATO,
         DS_GENERO,
 
@@ -30,20 +32,52 @@ tb_genero as (
 
     from tb_candidaturas
 
+),
+
+tb_cor_candidato as (
+select SQ_CANDIDATO,
+    SG_UF,
+    SG_PARTIDO,
+    case when DS_COR_RACA = 'BRANCA' then 1 else 0 end as cor_BRANCA,
+    case when DS_COR_RACA = 'PARDA' then 1 else 0 end as cor_PARDA,
+    case when DS_COR_RACA = 'PRETA' then 1 else 0 end as cor_PRETA,
+    case when DS_COR_RACA = 'NÃO INFORMADO' then 1 else 0 end as cor_NAO_INFORMADO,
+    case when DS_COR_RACA = 'INDÍGENA' then 1 else 0 end as cor_INDÍGENA,
+    case when DS_COR_RACA = 'AMARELA' then 1 else 0 end as cor_AMARELA,
+    case when DS_COR_RACA = 'NÃO DIVULGÁVEL' then 1 else 0 end as cor_DIVULGAVEL
+
+from tb_candidaturas
+),
+
+tb_cor_genero as (
+select t1.SQ_CANDIDATO,
+        t1.SG_PARTIDO,
+        t1.SG_UF,
+        t1.masc,
+        t1.fem,
+        t2.cor_BRANCA,
+        t2.cor_PARDA,
+        t2.cor_PRETA,
+        t2.cor_NAO_INFORMADO,
+        t2.cor_INDÍGENA,
+        t2.cor_AMARELA,
+        t2.cor_DIVULGAVEL
+
+from tb_genero as t1
+
+left join tb_cor_candidato as t2
+on t1.SQ_CANDIDATO = t2.SQ_CANDIDATO
 )
 
+
 select SG_PARTIDO,
-        sum(masc) as qtde_masc,
-        sum(fem) as qtde_fem,
-        count(SG_PARTIDO),
-        round(cast(sum(fem) as real) / cast(count(SG_PARTIDO) as real),2) as tx_fem
-
-from tb_genero
-
-group by SG_PARTIDO
-
-order by tx_fem desc
-
+    SG_UF,
+    sum(fem),
+    count(*),
+    sum(fem) / (1.0 * count(*)) as txfem,
+    
+from tb_cor_genero
+group by 1,2
 
 
 
